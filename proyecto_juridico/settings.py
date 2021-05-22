f6f2c50d6931c80385eacb2ba25312aae332f184
Nancyhdp <51838679+Nancyhdp@users.noreply.github.com>
2021-05-21 23:10:28 -0600

    new 1

--- proyecto_juridico/settings.py
+++ proyecto_juridico/settings.py
@@ -75,18 +75,12 @@ EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-DATABASES = {
 
-    'default': {
-        'ENGINE': 'sql_server.pyodbc',
-        'HOST': 'DESKTOP-8UJ281S',
-        'PORT': '1433',
-        'NAME': 'db_juridica',
-        'OPTIONS': {
-            'driver': 'ODBC Driver 17 for SQL Server',
-            'unicode_results': True,
-        },
-    },
+DATABASES = {
+    'default':{
+        'ENGINE':'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
 }
 
 AUTH_USER_MODEL='Sistema_juridico.Usuario'
