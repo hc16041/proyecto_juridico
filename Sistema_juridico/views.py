6cd2a1a5f77ee0d025c3b1aced7144f2223978a6
Fernando Castellanos <hc16041@ues.edu.sv>
2021-06-01 23:50:26 -0600

    cambios de subida

--- Sistema_juridico/views.py
+++ Sistema_juridico/views.py
@@ -1,10 +1,5 @@
 from django.shortcuts import render
 from django.urls import reverse_lazy
-<<<<<<< HEAD
-
-=======
-import requests
->>>>>>> 7b80ebe4cc9cf139acefe08173d02505c319a28b
 from django.views.generic import TemplateView, ListView, DetailView, CreateView, DeleteView, UpdateView, FormView,View
 from django.db.models import Q
 from django.http import HttpResponse, HttpResponseRedirect
@@ -393,7 +388,3 @@ def handler403(request,exception=None):
 
 def handler404(request,exception=None):
     return render(request,'errores/404.html')
-def index(request):
-    r = requests.get('http://httpbin.org/status/418')
-    print(r.text)
-    return HttpResponse('<pre>' + r.text + '</pre>')
